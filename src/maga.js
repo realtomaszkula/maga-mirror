e0a6db2f2d7730ce746a5fd13ad917dbe52f1086
Tomasz Kula <tomaszkula07@gmail.com>
2016-12-24 20:43:49 +0100

    Add env file

--- src/maga.js
+++ src/maga.js
@@ -1,1 +1,21 @@
-console.log('Make America Great Again!');
+const OAuth = require('oauth');
+const ENV = require('../env');
+
+console.log(ENV.CONSUMER_KEY);
+// const requestToken = '';
+// const accessToken = '';
+// const consumerKey = '';
+// const applicationSecret = '';::
+// 
+// let oauth = new OAuth.OAuth(
+//   requestToken,
+//   accessToken,
+//   consumerKey,
+//   applicationSecret,
+//   '1.0A',
+//   null,
+//   'HMAC-SHA1'
+// )
+// 
+// console.log(process.env.HOME);
+// 
