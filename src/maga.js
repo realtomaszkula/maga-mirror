e9769e091c924089356a99142503c141c82647b2
Tomasz Kula <tomaszkula07@gmail.com>
2016-12-25 07:48:40 +0100

    Updates with current fav_count, retweet_count, text, and time_ago

--- src/maga.js
+++ src/maga.js
@@ -1,5 +1,7 @@
 const Twitter = require('twitter');
 const ENV = require('../env');
+const timeAgo = require('../lib/timeAgo');
+
 const {consumer_key, consumer_secret, access_token_key, access_token_secret} = ENV;
 
 const client = new Twitter({
@@ -21,9 +23,8 @@ function parseTweet(tweet) {
 }
 
 function parseResponse(response) {
-  return Array.isArray(response) 
-          ? response.map(tweet => parseTweet(tweet))
-          : response.statuses.map(tweet => parseTweet(tweet));
+  console.log(response);
+  return response.map(tweet => parseTweet(tweet))
 }
 
 function parseProfileInfo(response) {
@@ -39,21 +40,12 @@ function parseProfileInfo(response) {
   }
 }
 
-let recentMentions = {
-  endpoint: 'search/tweets',
-  params: {
-    q: '@realDonaldTrump',
-    result_type: 'recent'
-  }
-}
-
 let donaldTimeline = {
   endpoint: 'statuses/user_timeline',
   params: {
     screen_name: 'realDonaldTrump',
-    count: 5,
+    count: 1,
     trim_user: true,
-    exclude_replies: true,
     include_rts: false
   }
 }
@@ -65,7 +57,6 @@ let profileStyles = {
   }
 }
 
-
 // target dom
 const whois = {
   name: document.querySelector('.whois__name'),
@@ -75,27 +66,54 @@ const whois = {
 
 const statistics = {
   reply:  document.querySelector('.statistics__reply'),
-  retweet: document.querySelector('.statistics__retweet'),
-  heart: document.querySelector('.statistics__like'),
+  retweet_count: document.querySelector('.statistics__retweet'),
+  favorite_count: document.querySelector('.statistics__like'),
 }
 
 const profile = {
   avatar: document.querySelector('.avatar')
 }
 
+const tweetContent = document.querySelector('.tweet-text');
+
+// render
+function formatDisplayedStatistics(value) {
+  return (value > 1000)
+    ? `${(value/1000).toFixed(1)}K`
+    : value
+}
+
+function formatDisplayedTweet(tweet) {
+  return tweet;
+}
+
+
+function formatDate(dateString) {
+  return timeAgo(new Date(dateString).getTime());
+}
+
 // hit api
 const handleResults = (response) => console.log(parseResponse(response));
 const handleError = (error) => console.error(error);
 
-getResponse(recentMentions.endpoint, recentMentions.params)
-   .then(handleResults, handleError);
-
 getResponse(donaldTimeline.endpoint, donaldTimeline.params)
-  .then(handleResults, handleError);
+  .then((resp) => {
+    const currentTweet = parseResponse(resp)[0];
+
+    // statistics
+    statistics.retweet_count.textContent = formatDisplayedStatistics(currentTweet.retweet_count);
+    statistics.favorite_count.textContent = formatDisplayedStatistics(currentTweet.favorite_count);
+    whois.timeAgo.textContent = formatDate(currentTweet.created_at); 
+      
+    // tweet content
+   tweetContent.textContent = formatDisplayedTweet(currentTweet.text); 
+
+  }, handleError);
 
 getResponse(profileStyles.endpoint, profileStyles.params)
   .then((resp) => {
     const profileInfo = parseProfileInfo(resp);
     
     profile.avatar.setAttribute('src', profileInfo.profile_image_url);
+
   }, handleError);
